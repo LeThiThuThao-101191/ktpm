e9973fa1078fe2689bcc3eee5fd21213a96701be
Thu Thao <thao101191@donga.edu.vn>
2025-02-23 18:37:38 +0700

    Cap nhat

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/duan', {
+        await mongoose.connect('mongodb+srv://thao101191:dBM8FYL4lcdSCyTX@cluster0.clm1j.mongodb.net/vegetable', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
